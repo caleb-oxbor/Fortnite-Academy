9c5d2b3896aa87b50daf36641d61961a98d7445d
Caleb Oxborough <caleb.oxborough@ufl.edu>
2024-04-04 17:31:07 -0400

    trying to push from correct account

--- main.py
+++ main.py
@@ -3,9 +3,7 @@ import time
 
 # design red-black tree to represent ordered map
 print("hello world!")
-print("trying to push to the right branch")
-print("trying again!")
-print("maybe it worked?")
+print("push from school account test")
 
 # come up with a custom hashing function for unordered map
 
