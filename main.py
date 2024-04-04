3141c784ee7be7c2a11b613d2a70b1025960f5a1
Caleb Oxborough <oxboroughcaleb@gmail.com>
2024-04-04 13:10:13 -0400

    trying to push to main instead of master

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import time
 
 # design red-black tree to represent ordered map
 print("hello world!")
+print("trying to push to the right branch")
 
 # come up with a custom hashing function for unordered map
 
