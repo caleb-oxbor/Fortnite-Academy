b2b2998bb27462aa5047ed465636e1738e283af9
Caleb Oxborough <caleb.oxborough@ufl.edu>
2024-04-17 15:49:12 -0400

    running main inserts everything into a tree and a map. the map is way faster! (time displays in microseconds)

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, render_template, send_file
-# from collections import OrderedDict edit: we probs won't need this because we're building from scratch
+from structures import RB_Tree, Hash_Map
 from player import Player
-# import time
+import time
 import csv
 
 
@@ -34,15 +34,38 @@ def get_data():
 if __name__ == "__main__":
 
     player_vec = []
+    tree = RB_Tree()
+    hash_map = Hash_Map()
 
     # read csv
     with open("Fortnite_players_stats.csv", 'r', encoding='utf-8') as file:
 
         # create reader object and skip the first line
-        csv_reader = csv.reader(file)
-        next(csv_reader)
+        csv_reader1 = csv.reader(file)
+        csv_reader2 = csv.reader(file)
+        csv_reader3 = csv.reader(file)
 
-        for row in csv_reader:
+        next(csv_reader1)
+        next(csv_reader2)
+        next(csv_reader3)
+
+        tree_start_time = time.perf_counter()
+
+        for row in csv_reader1:
+
+            tree.insert(Player(row[0],
+            row[1], row[2], row[3], row[4], row[5], row[6], row[7],
+            row[8], row[9], row[10], row[11], row[12], row[13], row[14],
+            row[15], row[16], row[17], row[18], row[19], row[20], row[21],
+            row[22], row[23], row[24], row[25], row[26], row[27], row[28],
+            row[29], row[30], row[31], row[32], row[33], row[34], row[35], row[36]))
+
+        tree_end_time = time.perf_counter()
+        tree_elapsed_time = tree_end_time - tree_start_time
+        print("time to build tree: ", (tree_elapsed_time * 1000000), "μs")
+
+        for row in csv_reader2:
+
             player_vec.append(Player(row[0],
             row[1], row[2], row[3], row[4], row[5], row[6], row[7],
             row[8], row[9], row[10], row[11], row[12], row[13], row[14],
@@ -50,8 +73,21 @@ if __name__ == "__main__":
             row[22], row[23], row[24], row[25], row[26], row[27], row[28],
             row[29], row[30], row[31], row[32], row[33], row[34], row[35], row[36]))
 
-    print("Done!")
-    app.run()  # uncomment to run the thing, but it's better to do "flask run" in terminal
-    # type " $env:FLASK_APP="main.py" " in terminal to get it working
+        hashmap_start_time = time.perf_counter()
 
-    # implement and time structures here
+        for row in csv_reader3:
+
+            hash_map.insert(Player(row[0],
+            row[1], row[2], row[3], row[4], row[5], row[6], row[7],
+            row[8], row[9], row[10], row[11], row[12], row[13], row[14],
+            row[15], row[16], row[17], row[18], row[19], row[20], row[21],
+            row[22], row[23], row[24], row[25], row[26], row[27], row[28],
+            row[29], row[30], row[31], row[32], row[33], row[34], row[35], row[36]))
+
+        hashmap_end_time = time.perf_counter()
+        hashmap_elapsed_time = hashmap_end_time - hashmap_start_time
+        print("time to build hashmap: ", (hashmap_elapsed_time * 1000000), "μs")
+
+    print("Done!")
+    app.run()
+    # type " $env:FLASK_APP="main.py" " to get it working in terminal
