4bd22eca835d2b31dadc82807ba60cffcfc74803
Caleb Oxborough <caleb.oxborough@ufl.edu>
2024-04-18 10:43:59 -0400

    refactored the main Flask operations into two files - now main.py is a script, and app.py is what you run to actually see the site. main.py will be doing all the calculations, and app.py will handle data routing

--- chart1.js
+++ chart1.js
@@ -1,26 +1,3 @@
-/* old dummy chart
-const ctx = document.getElementById('barchart');
-
-  new Chart(ctx, {
-    type: 'bar',
-    data: {
-      labels: ['X label1', 'etc', 'etC', 'etcs', 'etcss', 'etcsss'],
-      datasets: [{
-        label: 'Title here',
-        data: [12, 19, 3, 5, 2, 3],
-        borderWidth: 1
-      }]
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
-        }
-      }
-    }
-  });
-  */
-
 document.addEventListener('DOMContentLoaded', function() {
     // Fetch data from the backend
     fetch('/data')
--- main.py
+++ main.py
@@ -1,93 +1,62 @@
-from flask import Flask, jsonify, render_template, send_file
 from structures import RB_Tree, Hash_Map
 from player import Player
 import time
 import csv
 
+# this file contains the main operations and sends them to app.py
 
-app = Flask(__name__, template_folder='.')  # static_url_path=''
+player_vec = []
+tree = RB_Tree()
+hash_map = Hash_Map()
 
+# read csv
+with open("Fortnite_players_stats.csv", 'r', encoding='utf-8') as file:
+    # create reader objects and skip the first line for each
+    csv_reader1 = csv.reader(file)
+    csv_reader2 = csv.reader(file)
+    csv_reader3 = csv.reader(file)
 
-@app.route('/')
-def index():
-    return render_template("tempo.html")
+    next(csv_reader1)
+    next(csv_reader2)
+    next(csv_reader3)
 
+    tree_start_time = time.perf_counter()
 
-@app.route('/favicon.ico')
-def favicon():
-    return '', 204
+    for row in csv_reader1:
+        tree.insert(Player(row[0],
+                           row[1], row[2], row[3], row[4], row[5], row[6], row[7],
+                           row[8], row[9], row[10], row[11], row[12], row[13], row[14],
+                           row[15], row[16], row[17], row[18], row[19], row[20], row[21],
+                           row[22], row[23], row[24], row[25], row[26], row[27], row[28],
+                           row[29], row[30], row[31], row[32], row[33], row[34], row[35], row[36]))
 
+    tree_end_time = time.perf_counter()
+    tree_elapsed_time = tree_end_time - tree_start_time
+    # storing time in microseconds, perfect for seeing the difference between the structures
+    print("time to build tree: ", (tree_elapsed_time * 1000000), "μs")
 
-@app.route('/<path:filename>')
-def static_files(filename):
-    return send_file(filename)
+    for row in csv_reader2:
+        player_vec.append(Player(row[0],
+                                 row[1], row[2], row[3], row[4], row[5], row[6], row[7],
+                                 row[8], row[9], row[10], row[11], row[12], row[13], row[14],
+                                 row[15], row[16], row[17], row[18], row[19], row[20], row[21],
+                                 row[22], row[23], row[24], row[25], row[26], row[27], row[28],
+                                 row[29], row[30], row[31], row[32], row[33], row[34], row[35], row[36]))
 
+    hashmap_start_time = time.perf_counter()
 
-@app.route('/data')
-def get_data():
-    labels = ['A', 'B', 'C']
-    values = [10, 20, 45]
-    data = {'labels': labels, 'values': values}
-    return jsonify(data)
+    for row in csv_reader3:
+        hash_map.insert(Player(row[0],
+                               row[1], row[2], row[3], row[4], row[5], row[6], row[7],
+                               row[8], row[9], row[10], row[11], row[12], row[13], row[14],
+                               row[15], row[16], row[17], row[18], row[19], row[20], row[21],
+                               row[22], row[23], row[24], row[25], row[26], row[27], row[28],
+                               row[29], row[30], row[31], row[32], row[33], row[34], row[35], row[36]))
 
+    hashmap_end_time = time.perf_counter()
+    hashmap_elapsed_time = hashmap_end_time - hashmap_start_time
+    print("time to build hashmap: ", (hashmap_elapsed_time * 1000000), "μs")
 
-if __name__ == "__main__":
-
-    player_vec = []
-    tree = RB_Tree()
-    hash_map = Hash_Map()
-
-    # read csv
-    with open("Fortnite_players_stats.csv", 'r', encoding='utf-8') as file:
-
-        # create reader object and skip the first line
-        csv_reader1 = csv.reader(file)
-        csv_reader2 = csv.reader(file)
-        csv_reader3 = csv.reader(file)
-
-        next(csv_reader1)
-        next(csv_reader2)
-        next(csv_reader3)
-
-        tree_start_time = time.perf_counter()
-
-        for row in csv_reader1:
-
-            tree.insert(Player(row[0],
-            row[1], row[2], row[3], row[4], row[5], row[6], row[7],
-            row[8], row[9], row[10], row[11], row[12], row[13], row[14],
-            row[15], row[16], row[17], row[18], row[19], row[20], row[21],
-            row[22], row[23], row[24], row[25], row[26], row[27], row[28],
-            row[29], row[30], row[31], row[32], row[33], row[34], row[35], row[36]))
-
-        tree_end_time = time.perf_counter()
-        tree_elapsed_time = tree_end_time - tree_start_time
-        print("time to build tree: ", (tree_elapsed_time * 1000000), "μs")
-
-        for row in csv_reader2:
-
-            player_vec.append(Player(row[0],
-            row[1], row[2], row[3], row[4], row[5], row[6], row[7],
-            row[8], row[9], row[10], row[11], row[12], row[13], row[14],
-            row[15], row[16], row[17], row[18], row[19], row[20], row[21],
-            row[22], row[23], row[24], row[25], row[26], row[27], row[28],
-            row[29], row[30], row[31], row[32], row[33], row[34], row[35], row[36]))
-
-        hashmap_start_time = time.perf_counter()
-
-        for row in csv_reader3:
-
-            hash_map.insert(Player(row[0],
-            row[1], row[2], row[3], row[4], row[5], row[6], row[7],
-            row[8], row[9], row[10], row[11], row[12], row[13], row[14],
-            row[15], row[16], row[17], row[18], row[19], row[20], row[21],
-            row[22], row[23], row[24], row[25], row[26], row[27], row[28],
-            row[29], row[30], row[31], row[32], row[33], row[34], row[35], row[36]))
-
-        hashmap_end_time = time.perf_counter()
-        hashmap_elapsed_time = hashmap_end_time - hashmap_start_time
-        print("time to build hashmap: ", (hashmap_elapsed_time * 1000000), "μs")
-
-    print("Done!")
-    app.run()
-    # type  $env:FLASK_APP="main.py"  to get it working in terminal
+labels = ['A', 'B', 'C']
+values = [10, 20, 15]
+data = {'labels': labels, 'values': values}
